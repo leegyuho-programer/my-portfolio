3e52d4597a7fe11fc173f38bacad00ccdc592adc
Gyuho Lee <j980303@naver.com>
2025-07-11 17:34:17 +0900

    feat: 폰트 추가 및 초기 설정

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,7 @@
 const config = {
   plugins: {
     '@tailwindcss/postcss': {},
+    autoprefixer: {}, // 브라우저 호환성을 위한 Autoprefixer (선택 사항이지만 권장)
   },
 };
 export default config;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,70 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import LocalFont from 'next/font/local';
+
+// Noto Sans KR 폰트 정의
+// public 폴더를 기준으로 상대 경로를 지정합니다.
+const notoSansKr = LocalFont({
+  src: [
+    {
+      path: '../../public/fonts/noto-sans-kr-14-300-normal.woff2',
+      weight: '300', // Light
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/noto-sans-kr-14-500-normal.woff2',
+      weight: '500', // Medium
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/noto-sans-kr-16-300-normal.woff2',
+      weight: '300', // Light
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/noto-sans-kr-16-700-normal.woff2',
+      weight: '700', // Bold
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/noto-sans-kr-16-900-normal.woff2',
+      weight: '900', // Black
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/noto-sans-kr-20-300-normal.woff2',
+      weight: '300', // Light
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/noto-sans-kr-20-400-normal.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/noto-sans-kr-24-700-normal.woff2',
+      weight: '700', // Bold
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/noto-sans-kr-24-900-normal.woff2',
+      weight: '900', // Black
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/noto-sans-kr-48-900-normal.woff2',
+      weight: '900', // Black
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/noto-sans-kr-64-900-normal.woff2',
+      weight: '900', // Black
+      style: 'normal',
+    },
+  ],
+  display: 'swap', // 폰트 로딩 전략: 폰트 로딩 중에는 다른 폰트 표시 후 로드되면 교체
+  variable: '--font-noto-sans-kr', // Tailwind CSS에서 사용할 CSS 변수 이름
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -12,7 +77,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang='ko' className={`${notoSansKr.variable}`}>
       <body>{children}</body>
     </html>
   );
