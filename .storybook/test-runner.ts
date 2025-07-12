ce54c9b7b5b50c31eab8ab339e1863a8e290a4e6
Gyuho Lee <j980303@naver.com>
2025-07-12 17:54:23 +0900

    feat: test-runner 추가

---
+++ .storybook/test-runner.ts
@@ -0,0 +1,17 @@
+// .storybook/test-runner.ts
+import { injectAxe, checkA11y } from 'axe-playwright';
+import { TestRunnerConfig } from '@storybook/test-runner';
+
+const config: TestRunnerConfig = {
+  async preVisit(page) {
+    await injectAxe(page);
+  },
+  async postVisit(page, context) {
+    await checkA11y(page, '#root', {
+      detailedReport: true,
+      detailedReportOptions: { html: true },
+    });
+  },
+};
+
+export default config;
