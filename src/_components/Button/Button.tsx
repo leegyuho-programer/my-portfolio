242aabf9a73285b34774b89c177960b18d1e40b7
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2025-07-13 20:53:56 +0900

    feat: Link에 role, tabIndex 추가

--- src/_components/Button/Button.tsx
+++ src/_components/Button/Button.tsx
@@ -8,6 +8,8 @@ interface ButtonProps {
 export default function Button({ children }: ButtonProps) {
   return (
     <Link
+      role="button"
+      tabIndex={0}
       href='#about-me'
       className={`bg-accent hover:bg-[#0069d9] w-[130px] h-[50px] text-sm text-white rounded-primary-button ${flexCenter}`}
     >
