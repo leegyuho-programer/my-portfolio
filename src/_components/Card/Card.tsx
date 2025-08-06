01445ddcdc37e6579153481a79c20ff505848f5a
Gyuho Lee <j980303@naver.com>
2025-08-06 14:34:20 +0900

    feat: 브라우저에서도 이미지가 캐싱되도록 구현

--- src/_components/Card/Card.tsx
+++ src/_components/Card/Card.tsx
@@ -34,7 +34,13 @@ interface CardImageProps {
 Card.Image = function CardImage({ src, alt }: CardImageProps) {
   return (
     <div className='relative w-full h-[200px]'>
-      <Image src={src} alt={alt} className='rounded-t-lg object-cover' fill />
+      <Image
+        src={src}
+        alt={alt}
+        className='rounded-t-lg object-cover'
+        priority
+        fill
+      />
     </div>
   );
 };
