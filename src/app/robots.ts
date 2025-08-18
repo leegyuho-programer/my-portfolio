3aeacc3b5a702d75993d16e612f9a2cc8d99feb5
Gyuho Lee <j980303@naver.com>
2025-08-18 17:07:29 +0900

    fix: robots 파일 ts 확장자로 수정

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,8 +1,7 @@
 import type { MetadataRoute } from 'next';
-import {BASE_URL} from '../constants/index.ts';
+import { BASE_URL } from '../constants/index.js';
 
 export default function robots(): MetadataRoute.Robots {
-
   return {
     rules: {
       userAgent: '*', // 모든 크롤러 허용
