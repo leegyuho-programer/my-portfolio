a55a7a163923fe97795955bab56b92f99824d59f
Gyuho Lee <j980303@naver.com>
2025-08-18 16:53:20 +0900

    fix: 앵커 URL 제거

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -11,37 +11,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: `${BASE_URL}/`,
       lastModified: now,
       changeFrequency: 'monthly',
-      priority: 1.0,
-    },
-    {
-      url: `${BASE_URL}/#about-me`,
-      lastModified: now,
-      changeFrequency: 'yearly',
-      priority: 0.8,
-    },
-    {
-      url: `${BASE_URL}/#archiving`,
-      lastModified: now,
-      changeFrequency: 'yearly',
-      priority: 0.8,
-    },
-    {
-      url: `${BASE_URL}/#skills`,
-      lastModified: now,
-      changeFrequency: 'yearly',
-      priority: 0.8,
-    },
-    {
-      url: `${BASE_URL}/#projects`,
-      lastModified: now,
-      changeFrequency: 'monthly',
-      priority: 0.9,
-    },
-    {
-      url: `${BASE_URL}/#contact`,
-      lastModified: now,
-      changeFrequency: 'yearly',
-      priority: 0.7,
+      priority: 1,
     },
   ];
 
