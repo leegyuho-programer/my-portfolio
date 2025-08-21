a5d72b4bc1438f02479b50c00d5ed045bba26634
Gyuho Lee <j980303@naver.com>
2025-08-21 14:19:27 +0900

    feat: SkillsSection 반응형 추가

--- src/_components/Sections/SkillsSection.tsx
+++ src/_components/Sections/SkillsSection.tsx
@@ -6,7 +6,7 @@ export function SkillsSection() {
   return (
     <section id='skills' className={sectionStyle}>
       <h2 className={sectionTitle}>SKILLS</h2>
-      <div className='grid grid-cols-4 gap-[15px] w-fit mx-auto'>
+      <div className='grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 gap-[15px] w-fit mx-auto'>
         {TECH_SKILLS.map((tech, index) => (
           <TechStackBadge key={`skill-${tech}-${index}`} tech={tech} />
         ))}
