e90fcf2ea51c70c7dacf9fa97140529c58badc61
Gyuho Lee <j980303@naver.com>
2025-08-21 11:44:19 +0900

    feat: AboutMeSection 반응형 추가

--- src/app/styles.ts
+++ src/app/styles.ts
@@ -4,7 +4,7 @@ export const flexCenter = 'flex items-center justify-center';
 export const flexRowCenter = `${flexRow} items-center justify-center`;
 export const flexColCenter = `${flexCol} items-center justify-center`;
 export const sectionStyle = 'scroll-py-16 p-32 mx-auto';
-export const sectionTitle = 'text-xl font-bold text-white mb-50';
+export const sectionTitle = `lg:justify-start ${flexRowCenter} text-xl font-bold text-white mb-50`;
 export const modalSectionTitle = 'text-xl font-semibold mb-10';
 export const modalSectionSubTitle =
   'text-lg font-bold text-text-main mb-10 bg-mainGray p-3';
